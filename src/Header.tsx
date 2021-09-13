98b09d8e78b85ae3bfd405e1196d9ef807ef83da
Phaedrus Raznikov <praznikov@cerego.com>
2021-09-13 10:35:58 -0400

    Stub new post button

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 export const Header = () => {
+    const showCreateForm = () => {
+        console.log("STUB")
+    }
+
     return (
-        <div id="app-header">
+        <div id="app-header" style={styles.headerContainer as React.CSSProperties}>
             <h1 id="app-title">Brainstorm Press</h1>
+            <button style={styles.button} onClick={showCreateForm}>New Post</button>
         </div>
     );
 };
+
+const styles = {
+    headerContainer: {
+        display: "flex",
+        // Requires use with "as React.CSSProperties" because "row" is cast as a string
+        // and cannot be coerced into the string literal matching FlexDirectionProperty
+        // https://github.com/cssinjs/jss/issues/1344
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    button: {
+        marginRight: 10
+    }
+}
