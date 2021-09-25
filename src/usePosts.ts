4f581b537a793058081efd78bef48f35e2e0084f
Phaedrus Raznikov <praznikov@cerego.com>
2021-09-25 15:16:03 -0400

    Send JWT when fetching posts

--- src/usePosts.ts
+++ src/usePosts.ts
@@ -1,14 +1,22 @@
 import { useEffect, useState } from "react";
 import { TPost } from "./Post";
+import { useStore } from './store/zstore'
 
 export const usePosts = (): Array<TPost> => {
   const [posts, setPosts] = useState<Array<TPost>>([]);
 
+  const jwt: string = useStore(state => state.jwt)
+
   useEffect(() => {
-    fetch("/api/posts")
+    fetch("/api/posts", {
+      method: "GET",
+      headers: {
+        "Authorization": `Bearer ${jwt}`
+      }
+    })
       .then((res) => res.json())
       .then((json) => setPosts(json.data));
-  }, []);
+  }, [jwt]);
 
   return posts;
 };
