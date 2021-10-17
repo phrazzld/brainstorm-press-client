545b01204728771185dcec83f7a3bf00a6e495c6
Phaedrus Raznikov <praznikov@cerego.com>
2021-10-17 16:31:00 -0400

    Basic MUI on Settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,9 @@
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+import { useAccessToken } from "../hooks/useAccessToken";
 import { useNodeInfo } from "../hooks/useNodeInfo";
 import { useStore } from "../store/zstore";
 import { disconnectNode, rtaUpdateUser } from "../utils/api";
@@ -7,9 +11,11 @@ import { NodeInfo } from "../utils/types";
 
 export const Settings = () => {
     const user = useStore((state) => state.user);
-    const accessToken = useStore((state) => state.accessToken);
+    const accessToken = useAccessToken();
     const lndToken = useStore((state) => state.lndToken);
     const setLndToken = useStore((state) => state.setLndToken);
+
+    const [redirect, setRedirect] = useState<string>("");
 
     const [blogInputValue, setBlogInputValue] = useState<string>(
         user?.blog || ""
@@ -34,54 +40,72 @@ export const Settings = () => {
         if (!blogInputValue && user?.blog) {
             setBlogInputValue(user?.blog);
         }
-    }, [user]);
+    }, [user, blogInputValue]);
+
+    const goToConnectToLND = (): void => {
+        setRedirect("/connect-to-lnd");
+    };
+
+    if (redirect) {
+        return <Redirect to={redirect} />;
+    }
 
     // TODO: enable account deletion
     // TODO: enable editing name
     // TODO: enable adding email (for account recovery)
     return (
         <div id="settings-container">
-            <h2>Settings</h2>
-            <div id="account-container">
-                <p>Welcome {user?.name}</p>
-                <div className="form-text-input">
-                    <label>Blog Name: </label>
-                    <input
-                        type="text"
-                        name="blog"
-                        value={blogInputValue}
-                        onChange={handleBlogInputChange}
-                    />
-                    <div id="submit-edits">
-                        <button onClick={submitEdits}>Save Changes</button>
-                    </div>
-                </div>
-            </div>
-            <div id="lnd-container">
-                <div id="lnd-info-container"></div>
-                <div id="lnd-actions-container">
-                    {nodeInfo && (
-                        <p>
-                            Balance: {Number(nodeInfo.balance).toLocaleString()}{" "}
-                            sats
-                        </p>
-                    )}
-                    {!lndToken && (
-                        <Link to="/connect-to-lnd">Connect to LND</Link>
-                    )}
-                    {lndToken && (
-                        <a
-                            href="#"
-                            onClick={() => {
-                                disconnectNode(lndToken);
-                                setLndToken("");
-                            }}
-                        >
-                            Disconnect Node
-                        </a>
-                    )}
-                </div>
-            </div>
+            <Typography variant="h1" gutterBottom>
+                Settings
+            </Typography>
+
+            <Typography variant="body1" gutterBottom>
+                Welcome {user?.name}
+            </Typography>
+
+            <TextField
+                id="edit-blog-title"
+                label="Blog"
+                variant="filled"
+                onChange={handleBlogInputChange}
+                value={blogInputValue}
+                required
+            />
+
+            <br />
+            <br />
+
+            <Button variant="contained" onClick={submitEdits}>
+                Save Changes
+            </Button>
+
+            <br />
+            <br />
+
+            {nodeInfo && (
+                <Typography variant="body1" gutterBottom>
+                    Balance: {Number(nodeInfo.balance).toLocaleString()} sats
+                </Typography>
+            )}
+
+            {!lndToken && (
+                <Button variant="outlined" onClick={goToConnectToLND}>
+                    Connect to LND
+                </Button>
+            )}
+
+            {lndToken && (
+                <Button
+                    variant="outlined"
+                    color="error"
+                    onClick={() => {
+                        disconnectNode(lndToken);
+                        setLndToken("");
+                    }}
+                >
+                    Disconnect LND Node
+                </Button>
+            )}
         </div>
     );
 };
