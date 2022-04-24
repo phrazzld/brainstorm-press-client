8243a437676c83c2fdca4e2807b42864f2cc9779
Phaedrus Raznikov <praznikov@cerego.com>
2022-04-24 07:47:51 -0400

    Make subscriptions page the home page

--- src/App.tsx
+++ src/App.tsx
@@ -24,115 +24,109 @@ import { SignUp } from "./pages/SignUp";
 import { Subs } from "./pages/Subs";
 
 function App() {
-    return (
-        <Router>
-            <Header />
-            <div id="app-container" style={styles.appContainer}>
-                <Switch>
-                    <Route path="/signup">
-                        <Helmet>
-                            <title>Sign Up - Brainstorm Press</title>
-                        </Helmet>
-                        <SignUp />
-                    </Route>
-                    <Route path="/login">
-                        <Helmet>
-                            <title>Log In - Brainstorm Press</title>
-                        </Helmet>
-                        <LogIn />
-                    </Route>
-                    <Route path="/faq">
-                        <Helmet>
-                            <title>FAQ - Brainstorm Press</title>
-                        </Helmet>
-                        <Faq />
-                    </Route>
-                    <Route path="/reset-password/:userId/:token">
-                        <Helmet>
-                            <title>
-                                Create a New Password - Brainstorm Press
-                            </title>
-                        </Helmet>
-                        <ResetPassword />
-                    </Route>
-                    <Route path="/reset-password">
-                        <Helmet>
-                            <title>
-                                Reset Your Password - Brainstorm Press
-                            </title>
-                        </Helmet>
-                        <SendResetPasswordEmail />
-                    </Route>
-                    <Route path="/connect-to-ln">
-                        <Helmet>
-                            <title>
-                                Connect To Lightning - Brainstorm Press
-                            </title>
-                        </Helmet>
-                        <ConnectToLnForm />
-                    </Route>
-                    <Route path="/profile">
-                        <Helmet>
-                            <title>Profile - Brainstorm Press</title>
-                        </Helmet>
-                        <Profile />
-                    </Route>
-                    <Route path="/settings">
-                        <Helmet>
-                            <title>Settings - Brainstorm Press</title>
-                        </Helmet>
-                        <Settings />
-                    </Route>
-                    <Route path="/subscriptions">
-                        <Helmet>
-                            <title>Subscriptions - Brainstorm Press</title>
-                        </Helmet>
-                        <Subs />
-                    </Route>
-                    <Route path="/posts/new">
-                        <Helmet>
-                            <title>New Post - Brainstorm Press</title>
-                        </Helmet>
-                        <CreateNewPostForm />
-                    </Route>
-                    <Route path="/drafts">
-                        <Helmet>
-                            <title>Drafts - Brainstorm Press</title>
-                        </Helmet>
-                        <Drafts />
-                    </Route>
-                    <Route path="/posts/:postId/edit">
-                        <Helmet>
-                            <title>Edit Post - Brainstorm Press</title>
-                        </Helmet>
-                        <EditPost />
-                    </Route>
-                    <Route path="/posts/:postId">
-                        <Post />
-                    </Route>
-                    <Route path="/users/:username/blog">
-                        <Blog />
-                    </Route>
-                    <Route path="/">
-                        <Helmet>
-                            <title>Home - Brainstorm Press</title>
-                        </Helmet>
-                        <Home />
-                    </Route>
-                </Switch>
-            </div>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <div id="app-container" style={styles.appContainer}>
+        <Switch>
+          <Route path="/signup">
+            <Helmet>
+              <title>Sign Up - Brainstorm Press</title>
+            </Helmet>
+            <SignUp />
+          </Route>
+          <Route path="/login">
+            <Helmet>
+              <title>Log In - Brainstorm Press</title>
+            </Helmet>
+            <LogIn />
+          </Route>
+          <Route path="/faq">
+            <Helmet>
+              <title>FAQ - Brainstorm Press</title>
+            </Helmet>
+            <Faq />
+          </Route>
+          <Route path="/reset-password/:userId/:token">
+            <Helmet>
+              <title>Create a New Password - Brainstorm Press</title>
+            </Helmet>
+            <ResetPassword />
+          </Route>
+          <Route path="/reset-password">
+            <Helmet>
+              <title>Reset Your Password - Brainstorm Press</title>
+            </Helmet>
+            <SendResetPasswordEmail />
+          </Route>
+          <Route path="/connect-to-ln">
+            <Helmet>
+              <title>Connect To Lightning - Brainstorm Press</title>
+            </Helmet>
+            <ConnectToLnForm />
+          </Route>
+          <Route path="/profile">
+            <Helmet>
+              <title>Profile - Brainstorm Press</title>
+            </Helmet>
+            <Profile />
+          </Route>
+          <Route path="/settings">
+            <Helmet>
+              <title>Settings - Brainstorm Press</title>
+            </Helmet>
+            <Settings />
+          </Route>
+          <Route path="/subscriptions">
+            <Helmet>
+              <title>Subscriptions - Brainstorm Press</title>
+            </Helmet>
+            <Subs />
+          </Route>
+          <Route path="/posts/new">
+            <Helmet>
+              <title>New Post - Brainstorm Press</title>
+            </Helmet>
+            <CreateNewPostForm />
+          </Route>
+          <Route path="/drafts">
+            <Helmet>
+              <title>Drafts - Brainstorm Press</title>
+            </Helmet>
+            <Drafts />
+          </Route>
+          <Route path="/posts/:postId/edit">
+            <Helmet>
+              <title>Edit Post - Brainstorm Press</title>
+            </Helmet>
+            <EditPost />
+          </Route>
+          <Route path="/posts/:postId">
+            <Post />
+          </Route>
+          <Route path="/users/:username/blog">
+            <Blog />
+          </Route>
+          <Route path="/">
+            <Helmet>
+              <title>Subscriptions - Brainstorm Press</title>
+            </Helmet>
+            <Subs />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
+  );
 }
 
 const styles = {
-    appContainer: {
-        width: "75%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        marginBottom: "50px",
-    },
+  appContainer: {
+    width: "75%",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: "50px",
+  },
 };
 
 export default App;
