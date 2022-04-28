4b201eca180768f569aa8ea48e46056281966f05
Phaedrus Raznikov <praznikov@cerego.com>
2022-04-28 07:24:04 -0400

    Fix drafts path error

--- src/pages/CreateNewPostForm.tsx
+++ src/pages/CreateNewPostForm.tsx
@@ -5,11 +5,11 @@ import Switch from "@mui/material/Switch";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import {
-    convertToRaw,
-    DraftHandleValue,
-    Editor,
-    EditorState,
-    RichUtils,
+  convertToRaw,
+  DraftHandleValue,
+  Editor,
+  EditorState,
+  RichUtils,
 } from "draft-js";
 import "draft-js/dist/Draft.css";
 import React, { useState } from "react";
@@ -18,153 +18,139 @@ import { useAccessToken } from "../hooks/useAccessToken";
 import { rtaCreatePost } from "../utils/api";
 
 export const CreateNewPostForm = () => {
-    const [titleInputValue, setTitleInputValue] = useState<string>("");
-    const [premium, setPremium] = useState<boolean>(false);
-    const accessToken = useAccessToken();
+  const [titleInputValue, setTitleInputValue] = useState<string>("");
+  const [premium, setPremium] = useState<boolean>(false);
+  const accessToken = useAccessToken();
 
-    const [redirect, setRedirect] = useState<string>("");
+  const [redirect, setRedirect] = useState<string>("");
 
-    const [editorState, setEditorState] = useState(() =>
-        EditorState.createEmpty()
-    );
-    const editor: any = React.useRef(null);
-    const focusEditor = () => {
-        editor?.current?.focus();
+  const [editorState, setEditorState] = useState(() =>
+    EditorState.createEmpty()
+  );
+  const editor: any = React.useRef(null);
+  const focusEditor = () => {
+    editor?.current?.focus();
+  };
+
+  const handleTitleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setTitleInputValue(event.target.value);
+  };
+
+  const publishPost = async (): Promise<void> => {
+    const body = {
+      title: titleInputValue,
+      content: JSON.stringify(convertToRaw(editorState.getCurrentContent())),
+      premium: premium,
+      published: true,
     };
+    const newPost = await rtaCreatePost(body, accessToken);
+    setRedirect(`/posts/${newPost._id}`);
+  };
 
-    const handleTitleInputChange = (
-        event: React.ChangeEvent<HTMLInputElement>
-    ): void => {
-        setTitleInputValue(event.target.value);
+  const saveDraft = async (): Promise<void> => {
+    const body = {
+      title: titleInputValue,
+      content: JSON.stringify(convertToRaw(editorState.getCurrentContent())),
+      premium: premium,
+      published: false,
     };
+    await rtaCreatePost(body, accessToken);
+    setRedirect("/drafts");
+  };
 
-    const publishPost = async (): Promise<void> => {
-        const body = {
-            title: titleInputValue,
-            content: JSON.stringify(
-                convertToRaw(editorState.getCurrentContent())
-            ),
-            premium: premium,
-            published: true,
-        };
-        const newPost = await rtaCreatePost(body, accessToken);
-        setRedirect(`/posts/${newPost._id}`);
-    };
+  const cancelPost = (): void => {
+    setRedirect("/");
+  };
 
-    const saveDraft = async (): Promise<void> => {
-        const body = {
-            title: titleInputValue,
-            content: JSON.stringify(
-                convertToRaw(editorState.getCurrentContent())
-            ),
-            premium: premium,
-            published: false,
-        };
-        await rtaCreatePost(body, accessToken);
-        setRedirect("/posts/drafts");
-    };
+  const handleKeyCommand = (
+    command: string,
+    editorState: EditorState,
+    eventTimeStamp: number
+  ): DraftHandleValue => {
+    const newEditorState = RichUtils.handleKeyCommand(editorState, command);
 
-    const cancelPost = (): void => {
-        setRedirect("/");
-    };
-
-    const handleKeyCommand = (
-        command: string,
-        editorState: EditorState,
-        eventTimeStamp: number
-    ): DraftHandleValue => {
-        const newEditorState = RichUtils.handleKeyCommand(editorState, command);
-
-        if (newEditorState) {
-            setEditorState(newEditorState);
-            return "handled";
-        }
-
-        return "not-handled";
-    };
-
-    if (redirect) {
-        return <Redirect to={redirect} />;
+    if (newEditorState) {
+      setEditorState(newEditorState);
+      return "handled";
     }
 
-    return (
-        <div id="edit-post-form">
-            <Typography variant="h3" gutterBottom>
-                Create New Post
-            </Typography>
-            <TextField
-                id="edit-post-title"
-                label="Title"
-                variant="filled"
-                onChange={handleTitleInputChange}
-                value={titleInputValue}
-                style={styles.formField}
-                fullWidth
-                required
+    return "not-handled";
+  };
+
+  if (redirect) {
+    return <Redirect to={redirect} />;
+  }
+
+  return (
+    <div id="edit-post-form">
+      <Typography variant="h3" gutterBottom>
+        Create New Post
+      </Typography>
+      <TextField
+        id="edit-post-title"
+        label="Title"
+        variant="filled"
+        onChange={handleTitleInputChange}
+        value={titleInputValue}
+        style={styles.formField}
+        fullWidth
+        required
+      />
+
+      <div
+        style={{
+          minHeight: "6em",
+          cursor: "text",
+          marginTop: 20,
+          padding: 10,
+        }}
+        onClick={focusEditor}
+      >
+        <Editor
+          ref={editor}
+          editorState={editorState}
+          onChange={setEditorState}
+          handleKeyCommand={handleKeyCommand}
+          placeholder="Write something!"
+        />
+      </div>
+
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={premium}
+              onChange={(event) => setPremium(event.target.checked)}
+              name="premium"
+              color="primary"
             />
+          }
+          label="Premium"
+        />
+      </FormGroup>
 
-            <div
-                style={{
-                    minHeight: "6em",
-                    cursor: "text",
-                    marginTop: 20,
-                    padding: 10,
-                }}
-                onClick={focusEditor}
-            >
-                <Editor
-                    ref={editor}
-                    editorState={editorState}
-                    onChange={setEditorState}
-                    handleKeyCommand={handleKeyCommand}
-                    placeholder="Write something!"
-                />
-            </div>
-
-            <FormGroup>
-                <FormControlLabel
-                    control={
-                        <Switch
-                            checked={premium}
-                            onChange={(event) =>
-                                setPremium(event.target.checked)
-                            }
-                            name="premium"
-                            color="primary"
-                        />
-                    }
-                    label="Premium"
-                />
-            </FormGroup>
-
-            <div id="button-container" style={styles.formField}>
-                <Button
-                    variant="contained"
-                    onClick={publishPost}
-                    style={styles.button}
-                >
-                    Publish
-                </Button>
-                <Button
-                    variant="outlined"
-                    onClick={saveDraft}
-                    style={styles.button}
-                >
-                    Save Draft
-                </Button>
-                <Button onClick={cancelPost} style={styles.button}>
-                    Cancel
-                </Button>
-            </div>
-        </div>
-    );
+      <div id="button-container" style={styles.formField}>
+        <Button variant="contained" onClick={publishPost} style={styles.button}>
+          Publish
+        </Button>
+        <Button variant="outlined" onClick={saveDraft} style={styles.button}>
+          Save Draft
+        </Button>
+        <Button onClick={cancelPost} style={styles.button}>
+          Cancel
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 const styles = {
-    button: {
-        marginRight: 10,
-    },
-    formField: {
-        marginTop: 20,
-    },
+  button: {
+    marginRight: 10,
+  },
+  formField: {
+    marginTop: 20,
+  },
 };
